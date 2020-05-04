44cf22a778d3e1a5f85151174509b3dd7c74d8fb
kongzheng1993 <kongzheng1993@hotmail.com>
2020-05-04 16:53:59 +0800

    add discovery test

--- provider-payment8001/src/main/java/com/evil/cloud/controller/PaymentController.java
+++ provider-payment8001/src/main/java/com/evil/cloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.evil.cloud.entities.Payment;
 import com.evil.cloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -15,6 +18,9 @@ public class PaymentController {
 
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Value("${server.port}")
     private String serverPort;
@@ -42,7 +48,20 @@ public class PaymentController {
         } else {
             return new CommonResult(444, "查询失败，查询ID=" + id, null);
         }
+    }
 
+    @GetMapping("/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+
+        for (String service : services) {
+            log.info("####service: " + service);
+            List<ServiceInstance> instances = discoveryClient.getInstances(service);
+            for (ServiceInstance instance : instances) {
+                log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+            }
+        }
+        return this.discoveryClient;
     }
 
 }
